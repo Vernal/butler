98c31ad5b84f329244059f9a988a506f4808391f
Jason Lemieux <jason@Jasons-iMac.local>
2019-06-19 11:54:46 -0400

    initial dump

---
+++ wp-includes/js/l10n.js
@@ -0,0 +1,22 @@
+function convertEntities(b){var d,a;d=function(c){if(/&[^;]+;/.test(c)){var f=document.createElement("div");f.innerHTML=c;return !f.firstChild?c:f.firstChild.nodeValue}return c};if(typeof b==="string"){return d(b)}else{if(typeof b==="object"){for(a in b){if(typeof b[a]==="string"){b[a]=d(b[a])}}}}return b};
+/*
+     FILE ARCHIVED ON 19:29:23 Jul 11, 2018 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 19:13:50 Jun 10, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 126.833 (3)
+  esindex: 0.006
+  captures_list: 142.868
+  CDXLines.iter: 9.98 (3)
+  PetaboxLoader3.datanode: 104.32 (5)
+  exclusion.robots: 0.25
+  exclusion.robots.policy: 0.238
+  RedisCDXSource: 1.981
+  PetaboxLoader3.resolve: 475.209 (2)
+  load_resource: 536.667
+*/
